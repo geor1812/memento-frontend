7e77e20ff67cc9d21b0f03f0855a6ce4fdcda3bb
geor1812 <george.ursachi7@gmail.com>
2020-12-06 23:18:19 +0100

    More design

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -34,14 +34,24 @@ class Note extends Component {
             <div className="container mt-3" style={{
                 borderStyle: "solid",
                 borderColor: "#000000",
-                borderRadius: "25px",
+                borderRadius: "10px",
             }}>
-                <div className="row mt-3">
+                <div className="row m-3 bg-dark p-1" style={{
+                    borderStyle: "solid",
+                    borderColor: "#000000",
+                    borderRadius: "10px",
+                    borderWidth: "2px",
+                }}>
                     <div className="col-md-12">
                         <form className="form">
-                            <div className="form-group mb-2">
+                            <div className="form-group my-2">
                                 <label htmlFor="title" className="sr-only">Title</label>
-                                <textarea className="form-control-plaintext bg-danger text-light text-center" id="title" rows="1" onChange={this.onChangeTitle} required>{this.state.title}</textarea>
+                                <textarea className="form-control-plaintext bg-danger text-light text-center" id="title" rows="1"
+                                          style={{
+                                              borderStyle: "inset",
+                                              borderColor: "#000000",
+                                              borderWidth: "1px",
+                                          }} onChange={this.onChangeTitle} required>{this.state.title}</textarea>
                             </div>
                             <div className="container">
                                     <div className="row">
@@ -64,7 +74,8 @@ class Note extends Component {
                                 <textarea className="form-control bg-dark text-light" style={{
                                     borderStyle: "solid",
                                     borderColor: "#000000",
-                                    borderWidth: "2px"
+                                    borderWidth: "2px",
+                                    borderRadius: "10px"
                                 }} id="content" rows="20" onChange={this.onChangeContent}>{this.state.content}</textarea>
                             </div>
                         </form>
